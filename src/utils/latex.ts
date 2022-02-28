c192f808c4915a1e9df56820a96beba6ca950cdb
Leon Si <contact@leonzalion.com>
2022-02-28 11:40:46 -0500

    fix: symbolic link detection

--- src/utils/latex.ts
+++ src/utils/latex.ts
@@ -110,7 +110,7 @@ export function compileLatex({
 
 		// Copy all the temp files into the output directory
 		for (const tempFile of entriesToCopy) {
-			if (fs.statSync(tempFile).isSymbolicLink()) continue;
+			if (fs.lstatSync(tempFile).isSymbolicLink()) continue;
 			fs.cpSync(tempFile, path.join(outputDirectory, tempFile), {
 				recursive: true,
 			});
@@ -132,7 +132,7 @@ export function compileLatex({
 
 		// Copy all the temp files into the output directory
 		for (const tempFile of entriesToCopy) {
-			if (fs.statSync(tempFile).isSymbolicLink()) continue;
+			if (fs.lstatSync(tempFile).isSymbolicLink()) continue;
 			fs.cpSync(tempFile, path.join(outputDirectory, tempFile), {
 				recursive: true,
 			});
