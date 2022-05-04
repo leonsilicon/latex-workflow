4766cd0708253d60f9823189f762d6d556f30543
Leon Si <contact@leonzalion.com>
2022-05-04 17:48:08 -0400

    chore: update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,8 @@
-const path = require('path');
+const createESLintConfig = require('@leonzalion/configs/eslint.cjs');
 
-module.exports = {
-	extends: [require.resolve('@leonzalion/configs/eslint.cjs')],
-	parserOptions: { project: [path.resolve(__dirname, 'tsconfig.eslint.json')] },
+module.exports = createESLintConfig(__dirname, {
 	rules: {
-		'unicorn/no-process-exit': 'off',
-	},
-};
+		'unicorn/no-process-exit': 'off'
+	}
+});
+
